6308b2a43f33c009d655377f361cea5547781599
Zhongtao Yang <yzhongta@amazon.com>
2019-05-11 18:37:24 -0700

    Add simple components

---
+++ myapp/src/components/simplecomponents/SimpleComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function FirstComponent() {
+  return (
+    <>
+      <h1>First heading in third Component </h1>
+      <h1>Second heading in third Component </h1>
+    </>
+  )
+}
+
+export function SecondComponent() {
+  return (
+    <div>
+      Second Component
+    </div>
+  )
+}
+
+export default FirstComponent
